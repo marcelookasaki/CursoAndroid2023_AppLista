1b16e1bbe12abe3b7ccf9d374b045d239d88040b
Marcelo Yutaka Okasaki <marcelo.okasaki@gmail.com>
2023-05-30 17:00:48 -0300

    Classe Pessoa criada

--- app/src/main/java/dev/omy/applistacurso/model/Pessoa.java
+++ app/src/main/java/dev/omy/applistacurso/model/Pessoa.java
@@ -1,4 +1,43 @@
 package dev.omy.applistacurso.model;
 
 public class Pessoa {
+
+    private String primeiroNome;
+    private String sobreNome;
+    private String cursoDesejado;
+    private String telContato;
+
+    public Pessoa(){}
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelContato() {
+        return telContato;
+    }
+
+    public void setTelContato(String telContato) {
+        this.telContato = telContato;
+    }
 }
