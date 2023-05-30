58fe1b221804b9bcf97a3f5ad3c35eb467c55d22
Marcelo Yutaka Okasaki <marcelo.okasaki@gmail.com>
2023-05-30 16:34:57 -0300

    Classe Pessoa criada

--- app/src/main/java/dev/omy/applistacurso/model/Pessoa.java
+++ app/src/main/java/dev/omy/applistacurso/model/Pessoa.java
@@ -1,4 +1,41 @@
 package dev.omy.applistacurso.model;
 
 public class Pessoa {
+
+    private String primeiroNome, sobreNome, cursoDesejado, telContato;
+
+    public Pessoa() {}
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelContato() {
+        return telContato;
+    }
+
+    public void setTelContato(String telContato) {
+        this.telContato = telContato;
+    }
+
 }
--- app/src/main/java/dev/omy/applistacurso/view/MainActivity.java
+++ app/src/main/java/dev/omy/applistacurso/view/MainActivity.java
@@ -3,14 +3,38 @@ package dev.omy.applistacurso.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import dev.omy.applistacurso.R;
+import dev.omy.applistacurso.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv_appNome;
+    EditText editText_nome;
+    EditText editText_sobreNome;
+    EditText editText_cursoDesejado;
+    EditText editText_telContato;
+    Button btn_limpar, btn_salvar, btn_finalizar;
+    Pessoa pessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_appNome.findViewById(R.id.tv_appNome);
+        editText_nome.findViewById(R.id.editText_nome);
+        editText_sobreNome.findViewById(R.id.editText_sobreNome);
+        editText_cursoDesejado.findViewById(R.id.editText_cursoDesejado);
+        editText_telContato.findViewById(R.id.editText_telContato);
+        btn_limpar.findViewById(R.id.btn_limpar);
+        btn_salvar.findViewById(R.id.btn_salvar);
+        btn_finalizar.findViewById(R.id.btn_finalizar);
+
+        pessoa = new Pessoa();
+
     }
 }
